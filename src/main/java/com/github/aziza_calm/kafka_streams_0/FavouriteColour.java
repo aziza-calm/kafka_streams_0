df73999f9bc7e365c653d4d6a6fe4b7eef3803f7
aziza-calm <aziza.kalmasova@phystech.edu>
2020-02-28 18:50:14 +0300

    favourite colour almost finished, just some strange errors with serialization

--- src/main/java/com/github/aziza_calm/kafka_streams_0/FavouriteColour.java
+++ src/main/java/com/github/aziza_calm/kafka_streams_0/FavouriteColour.java
@@ -3,10 +3,10 @@ package com.github.aziza_calm.kafka_streams_0;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.*;
 
 import java.util.Properties;
 
@@ -20,10 +20,26 @@ public class FavouriteColour {
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         StreamsBuilder builder = new StreamsBuilder();
-        KTable<Object, Object> favColinput = builder.table("favcol-input");
-        KTable<Object, Object> favColCount = favColinput.mapValues(value -> value.toString().toLowerCase());
+        KStream<String, String> favColinput = builder.stream("favcol-input");
 
-        favColCount.toStream().to("favcol-output");
+        KStream<String, String> lol = favColinput
+                .filter((key, value) -> value.contains(","))
+                .mapValues(value -> value.toLowerCase())
+                .map((key, value) -> {
+                    String[] keyval = value.split(" ");
+                    if (keyval.length > 1) {
+                        KeyValue<String, String> result = KeyValue.pair(keyval[0], keyval[1]);
+                        return result;
+                    }
+                    return KeyValue.pair("null", "null");
+                })
+                .filter((key, value) -> (value != "green" && value != "red" && value != "blue"));
+        lol.to("temp-topic");
+        KTable<String, String> mytable = builder.table("temp-topic");
+        KTable<String, Long> finisch = mytable
+                .groupBy((key, value) -> new KeyValue<>(value, 1))
+                .count();
+        finisch.toStream().to("favcol-output", Produced.with(Serdes.String(), Serdes.Long()));
         KafkaStreams streams = new KafkaStreams(builder.build(), config);
         streams.start();
         //printed the topology
